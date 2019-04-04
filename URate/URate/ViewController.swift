d3bb0c1469c9d6dfa063049a92d67766722ac46d
Sahil Gogna <gogna.sahil95@gmail.com>
2019-04-03 20:00:24 -0400

    initial commit with basic entities

---
+++ URate/URate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URate
+//
+//  Created by Sahil Gogna on 2019-04-03.
+//  Copyright © 2019 Sahil Gogna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
